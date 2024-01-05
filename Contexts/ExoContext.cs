3128d783c7a33de46c85bc5ca2559ca98b0f3ea9
fausto <faustoapostolic@gmail.com>
2024-01-05 15:41:44 -0300

    first commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,34 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) :
+        base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder
+        optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Essa string de conexão depende da SUA máquina.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-IUNMD14\\SQLEXPRESS;Initial Catalog=ExoApi;Integrated Security=SSPI;Persist Security Info=False;");
+                // Exemplo 1 de string de conexão:
+                // User
+                //ID = sa; Password = admin; Server = localhost; Database = ExoApi; -
+                // + Trusted_Connection=False;
+                // Exemplo 2 de string de conexão:
+                // 
+                //Server = localhost\\SQLEXPRESS; Database = ExoApi; Trusted_Connection = True;
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
